abb9310bfc3aead90fa7beff36f46f5d93f80c3d
ronterambabu <rambabu.r@zynlogic.com>
2025-08-05 17:58:37 +0530

    Enable method security and web security in GlobalEventApplication

--- src/main/java/com/zn/GlobalEventApplication.java
+++ src/main/java/com/zn/GlobalEventApplication.java
@@ -2,8 +2,12 @@ package com.zn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
+@EnableWebSecurity
 public class GlobalEventApplication {
 
 	public static void main(String[] args) {
